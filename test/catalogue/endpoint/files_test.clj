fe570f2449963def6d664b828139e45dee955d69
Joel Gregory <jgregors@gmail.com>
2016-03-04 17:30:23 +1100

    added files stub

--- test/catalogue/endpoint/files_test.clj
+++ test/catalogue/endpoint/files_test.clj
@@ -10,29 +10,42 @@
             [ring.mock.request :as mock]
             [cheshire.core :as cheshire]))
 
-(def db (atom {}))
+(def db-component (atom {}))
 
-(defn insert-detail-stub [db]
-  (mc/insert-and-return (:db @db) "detail"
-             {:id "land-solution"
-              :owner {:name "Land Solution"
-                      :contact {:email "lee.hellen@landsolution.com.au"
-                                :telephone "07332933982"}}}))
+(def detail-stub {:id "land-solution"
+                  :owner {:name "Land Solution"
+                          :contact {:email "lee.hellen@landsolution.com.au"
+                                    :telephone "07332933982"}}})
 
-(defn remove-setup [db]
-  (mc/remove (:db @db) "detail" {}))
+(def files-stub [{:id "1"
+                   :location {:latitude -28.73
+                              :longitude 153.467864}}
+                  {:id "2"
+                   :location {:latitude -28.73
+                              :longitude 153.567864}}])
+
+(defn insert-stub [db-comp coll data]
+  (let [db (:db @db-comp)]
+    (if (vector? data)
+      (mc/insert-batch db coll data)
+      (mc/insert-and-return db coll data))))
+
+(defn remove-setup [db-comp]
+  (mc/remove (:db @db-comp) "files" {})
+  (mc/remove (:db @db-comp) "detail" {}))
 
 (use-fixtures :once
   (fn [f]
-    (reset! db (component/start (new-mongo-db)))
+    (reset! db-component (component/start (new-mongo-db)))
     (f)
-    (component/stop @db)))
+    (component/stop @db-component)))
 
 (use-fixtures :each
   (fn [f]
-    (insert-detail-stub db)
+    (insert-stub db-component "detail" detail-stub)
+    (insert-stub db-component "files" files-stub)
     (f)
-    (remove-setup db)))
+    (remove-setup db-component)))
 
 (defn handler [db]
   (files/files-endpoint {:db db}))
@@ -42,35 +55,43 @@
 
 (deftest catalogue-api
   (testing "catalogue resource exists"
-    (-> (session (handler @db))
+    (-> (session (handler @db-component))
         (visit "/api/catalogue")
         (has (status? 200) "catalogue resource does not exist")))
 
   (testing "files resource exists"
-    (-> (session (handler @db))
+    (-> (session (handler @db-component))
         (visit "/api/catalogue/files")
         (has (status? 200) "files resource does not exist"))))
 
 (deftest catalogue-test
   (testing "GET catalogue resource returns entire catalogue"
-    (let [response ((handler @db) (mock/request :get "/api/catalogue"))
+    (let [response ((handler @db-component) (mock/request :get "/api/catalogue"))
           body (parse-body (:body response))]
       (is (= (response :status) 200))
       (is (= (get-in response [:headers "Content-Type"])
              "application/json; charset=utf-8"))
-      (is (= [:detail :files] (keys body)))))
+      (is (= {:detail detail-stub :files files-stub} body))))
 
   (testing "GET catalogue detail resource returns correct information"
-    (let [response ((handler @db) (mock/request :get "/api/catalogue/detail"))
+    (let [response ((handler @db-component) (mock/request :get "/api/catalogue/detail"))
           body (parse-body (:body response))]
       (is (= (response :status) 200))
       (is (= (get-in response [:headers "Content-Type"])
              "application/json; charset=utf-8"))
-      (is (= [:id :owner] (keys body)))))
+      (is (= detail-stub body)))))
 
+(deftest db-methods
+  (testing "db-find does not return mongo _id"
+    (let [result (files/db-find @db-component "files" {})]
+      (is (not-any? #(= :_id %) (mapcat keys result)))))
+  
   (testing "db-find does not throw null pointer exceptions"
     (files/db-find nil nil nil))
 
+  (testing "db-find-one does not return mongo _id"
+    (let [result (files/db-find @db-component "files" {})]
+      (is (not-any? #(= :_id %) (keys result)))))  
+  
   (testing "db-find-one does not throw null pointer exceptions"
-    (files/db-find-one nil nil nil))))
-
+    (files/db-find-one nil nil nil)))
